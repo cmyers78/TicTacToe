0b34d39c4d10cdf2267df93e0ef7dec5c50b6c4c
Chris Myers <cmyers78@gmail.com>
2016-07-05 22:45:04 -0600

    Figured out how to use a for loop and button tags to assign images to buttons.   Fun activity!

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -37,15 +37,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.buttonZero.setImage(xImage, forState: .Normal)
-        self.buttonOne.setImage(oImage, forState: .Normal)
-        self.buttonTwo.setImage(xImage, forState: .Normal)
-        self.buttonThree.setImage(oImage, forState: .Normal)
-        self.buttonFour.setImage(xImage, forState: .Normal)
-        self.buttonFive.setImage(xImage, forState: .Normal)
-        self.buttonSix.setImage(xImage, forState: .Normal)
-        self.buttonSeven.setImage(oImage, forState: .Normal)
-        self.buttonEight.setImage(xImage, forState: .Normal)
+        for (index, values) in valuesArray.enumerate() {
+            
+            if values == "x" {
+                if let button = self.view.viewWithTag(index) as? UIButton {
+                    button.setImage(xImage, forState: .Normal)
+                print(index)
+                }
+            } else {
+                if let button = self.view.viewWithTag(index) as? UIButton {
+                    button.setImage(oImage, forState: .Normal)
+                print(index)
+                }
+            }
+        }
+        
+//        self.buttonZero.setImage(xImage, forState: .Normal)
+//        self.buttonOne.setImage(oImage, forState: .Normal)
+//        self.buttonTwo.setImage(xImage, forState: .Normal)
+//        self.buttonThree.setImage(oImage, forState: .Normal)
+//        self.buttonFour.setImage(xImage, forState: .Normal)
+//        self.buttonFive.setImage(xImage, forState: .Normal)
+//        self.buttonSix.setImage(xImage, forState: .Normal)
+//        self.buttonSeven.setImage(oImage, forState: .Normal)
+//        self.buttonEight.setImage(xImage, forState: .Normal)
         
         // Do any additional setup after loading the view, typically from a nib.
     }
